593f6d16d72f93cb0638356c75533a0dcc8e1e15
DevCompleteStudios <jorge.yael333@gmail.com>
2024-12-30 08:53:36 -0600

    first commit

---
+++ src/main/java/com/devstudios/store/app/purchase/products/store_purchase_products/StorePurchaseProductsApplication.java
@@ -0,0 +1,13 @@
+package com.devstudios.store.app.purchase.products.store_purchase_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorePurchaseProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorePurchaseProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devstudios/store/app/purchase/products/store_purchase_products/StorePurchaseProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devstudios.store.app.purchase.products.store_purchase_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorePurchaseProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
